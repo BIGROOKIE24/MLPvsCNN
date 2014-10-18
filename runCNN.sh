66c9471998f88d34ab8044d47b4693c1f731c1b0
Volkan Cirik <vcirik@ghc81.ghc.andrew.cmu.edu>
2014-10-18 17:08:13 -0400

    linked filed removed.

--- runCNN.sh
+++ runCNN.sh
@@ -1,1 +1,16 @@
-../LAB2/runCNN.sh
+#!/usr/bin/sh
+script=CNNwithAutoencoder.py
+n_epochs=200
+batch_size=500
+nkerns0=20
+nkerns1=50
+filtering=5
+poolsize=2
+for l_rate in "0.1" "0.2" "0.3" "0.4" "0.5"
+do
+    for hidden_size in {300..1100..200}
+    do
+#	eval "THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python $script $l_rate $n_epochs $batch_size $nkerns0 $nkerns1 $filtering $poolsize $hidden_size" > exp-logs/exp.log.$l_rate.$hidden_size 2> exp-logs/exp.err.$l_rate.$hidden_size
+	echo "THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python $script $l_rate $n_epochs $batch_size $nkerns0 $nkerns1 $filtering $poolsize $hidden_size"
+    done
+done
